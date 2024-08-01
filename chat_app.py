ff94f5a05a9e74e2d1a46540af3166fc0a9ec8b8
sasidharreddy-10 <70102908+sasidharreddy-10@users.noreply.github.com>
2024-08-01 19:40:44 +0530

    Initial Commit

---
+++ chat_app.py
@@ -0,0 +1,191 @@
+import os
+import time
+from io import BytesIO
+import streamlit as st
+from rag_pipeline import RAG, TextProcessor
+
+#Initialize TextProcessor class
+textprocessor=TextProcessor(chunk_size=1000, chunk_overlap=20)
+
+# Set an environment variable
+os.environ['PINECONE_API_KEY'] = '193bfd5b-1a4a-4c8b-bc06-7ec7c4cfc66a'
+
+PINECONE_API_KEY="193bfd5b-1a4a-4c8b-bc06-7ec7c4cfc66a"
+
+# Set the page title and layout
+st.set_page_config(page_title="Chatbot Interface", layout="wide")
+
+# Initialize session state variables
+if 'api_key' not in st.session_state:
+    st.session_state.api_key = ''
+if 'endpoint' not in st.session_state:
+    st.session_state.endpoint = ''
+if 'version' not in st.session_state:
+    st.session_state.version = ''
+if 'model_name' not in st.session_state:
+    st.session_state.model_name = ''
+if 'embedding_model_name' not in st.session_state:
+    st.session_state.embedding_model_name = ''
+if 'openai_type' not in st.session_state:
+    st.session_state.openai_type = ''
+if 'selected_files' not in st.session_state:
+    st.session_state.selected_files = []
+if 'uploaded_files' not in st.session_state:
+    st.session_state.uploaded_files = []
+
+# Function to display the modal
+def display_modal():
+    with st.expander("Please enter your Model Details:", expanded=False):
+        # User selection for OpenAI or Azure OpenAI
+        service = st.selectbox("Select Service", ["OpenAI", "Azure OpenAI"])
+        with st.form(key='model_details_form'):
+            if service == "OpenAI":
+                st.session_state.openai_type = "openai"
+                st.session_state.api_key = st.text_input("API Key", type="password")
+                st.session_state.model_name = st.text_input("Model Name")
+                st.session_state.embedding_model_name = st.text_input("Embedding Model Name")
+            elif service == "Azure OpenAI":
+                st.session_state.openai_type = "azure_openai"
+                st.session_state.api_key = st.text_input("API Key", type="password")
+                st.session_state.endpoint = st.text_input("Endpoint")
+                st.session_state.version = st.text_input("Version")
+                st.session_state.model_name = st.text_input("Model Name")
+                st.session_state.embedding_model_name = st.text_input("Embedding Model Name")
+
+            if st.form_submit_button("Submit"):
+                # Collect and display the input values
+                st.success("Details submitted successfully!")
+
+# Display the modal when the app starts
+display_modal()
+
+# Function to show notification
+def show_notification(message, message_type="success"):
+    # Define CSS styles for the notification
+    css = f"""
+    <style>
+    .notification {{
+        position: fixed;
+        top: 0;
+        right: 0;
+        margin: 20px;
+        padding: 10px 20px;
+        color: white; /* Text color */
+        border-radius: 5px;
+        z-index: 1000; /* Ensure it's on top */
+        background-color: {"#4CAF50" if message_type == "success" else "#f44336"}; /* Green for success, red for error */
+    }}
+    </style>
+    """
+    notification_placeholder = st.empty()
+    notification_placeholder.markdown(f'{css}<div class="notification">{message}</div>', unsafe_allow_html=True)
+    time.sleep(5)
+    notification_placeholder.empty()
+
+
+if st.session_state.api_key:
+    openai_type=st.session_state.openai_type
+    gpt_engine_name=st.session_state.model_name
+    embedding_model_name=st.session_state.embedding_model_name
+    azure_endpoint = st.session_state.endpoint
+    api_key=st.session_state.api_key
+    api_version= st.session_state.version
+    rag_obj=RAG('indextext', textprocessor, PINECONE_API_KEY, gpt_engine_name, embedding_model_name, api_key, azure_endpoint, api_version, openai_type)
+
+
+# Initialize chat history in session state if not already present
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+if "input_key" not in st.session_state:
+    st.session_state.input_key = 0
+
+uploaded_files = None
+selected_file = None
+
+
+# Handle file uploads only when user interacts with the sidebar
+with st.sidebar:
+    uploaded_files = st.file_uploader('Upload files', type=['pdf'], accept_multiple_files=True, label_visibility="hidden")
+
+    # condition=st.session_state.uploaded_files==uploaded_files
+    # if condition:
+    for uploaded_file in uploaded_files:
+        st.write("Select files:")
+        if uploaded_file not in st.session_state.uploaded_files:
+            bytes_data = uploaded_file.read()
+            file_like_object = BytesIO(bytes_data)
+            file_name=uploaded_file.name
+            try:
+                if st.session_state.api_key:
+                    show_notification(f"{uploaded_file.name} preprocessing started, Please wait for a while!")
+                    rag_obj.insert_doc(file_like_object, file_name)
+                    show_notification(f"{uploaded_file.name} inserted successfully!")
+                    if st.checkbox(uploaded_file.name):
+                        st.session_state.selected_files.append(uploaded_file)
+                    st.session_state.uploaded_files.append(uploaded_file)
+                else:
+                    show_notification("Please enter your openai credentails before uploading documents!", message_type='error')
+            except Exception as e:
+                show_notification("Something went wrong while preprocessing, please try again!", message_type='error')
+        else:
+            if st.checkbox(uploaded_file.name):
+                st.session_state.selected_files.append(uploaded_file)
+
+
+# Main content area
+st.markdown("# Chatbot Interface")
+st.markdown("### What can I help you with today?")
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
+    st.button("💡 Activities to make friends in new city")
+
+with col2:
+    st.button("✏️ Text inviting friend to wedding")
+
+with col3:
+    st.button("✈️ Experience Seoul like a local")
+
+with col4:
+    st.button("🖥️ Make me a personal webpage")
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat history
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    try:
+        if api_key:
+            answer=rag_obj.qna(prompt)
+            with st.chat_message("BOT"):
+                st.markdown(answer)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "Bot", "content": answer})
+        else:
+            st.error("Please enter your API Key and other details at the top.")
+    except Exception as e:
+        answer="Something went wrong, please check your openai credentials"
+        st.error(answer)
+
+# Dummy element to trigger auto-scrolling
+auto_scroll = st.empty()
+
+# Trigger auto-scroll by adding an empty message after all other messages
+with auto_scroll:
+    st.write("")  # This empty write forces the page to render and auto-scroll
---
+++ rag_pipeline.py
@@ -0,0 +1,120 @@
+# import
+import os
+import time
+import PyPDF2
+from typing import Any
+from openai import OpenAI
+from openai import AzureOpenAI
+from pinecone import Pinecone, ServerlessSpec
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+class TextProcessor:
+    def __init__(self, chunk_size=1000, chunk_overlap=20):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap,
+            length_function=len,
+            is_separator_regex=False,
+        )
+    def _extract_text(self, file_path):
+        try:
+            reader = PyPDF2.PdfReader(file_path)
+            text = ""
+            for page_num in range(len(reader.pages)):
+                page = reader.pages[page_num]
+                text += page.extract_text() if page.extract_text() else ""
+            return text
+        except Exception as e:
+            print(f"Error reading PDF file: {e}")
+            raise Exception("Error in PDF reading")
+    def split_text(self, file_path, file_name):
+        try:
+            text=self._extract_text(file_path)
+            docs=self.text_splitter.create_documents([text], metadatas=[{"filename":file_name}])
+            return docs
+        except Exception as e:
+            print(f"Error splitting PDF text: {e}")
+            raise Exception("Error in PDF text splitting")
+        
+class RAG:
+    def __init__(self, index_name, text_processor : TextProcessor, PINECONE_API_KEY, gpt_engine_name, embedding_model_name, api_key, azure_endpoint, api_version, openai_type) -> None:
+        self.index_name = index_name
+        self.docsearch=None
+        self.gpt_engine_name=gpt_engine_name
+        self.doc_processing=text_processor
+        self.pc = Pinecone(api_key=PINECONE_API_KEY)
+        if openai_type=='azure_openai':
+            self.openai_client = AzureOpenAI(
+                    azure_endpoint = azure_endpoint,
+                    api_key=api_key,
+                    api_version= api_version
+                )
+            self.embeddings = AzureOpenAIEmbeddings(
+                model=embedding_model_name, 
+                api_key=api_key, 
+                azure_endpoint=azure_endpoint, 
+                openai_api_version=api_version)
+        else:
+            self.openai_client = OpenAI(api_key=api_key)
+            self.embeddings = OpenAI(api_key=api_key)
+        self.__call__()
+        
+    def __call__(self) -> None:
+        existing_indexes = [index_info["name"] for index_info in self.pc.list_indexes()]
+        if self.index_name not in existing_indexes:
+            self.pc.create_index(
+                name=self.index_name,
+                dimension=1536,
+                metric="cosine",
+                spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+            )
+            while not self.pc.describe_index(self.index_name).status["ready"]:
+                time.sleep(1)
+        self.docsearch = PineconeVectorStore(index_name=self.index_name, embedding=self.embeddings)
+
+    def insert_doc(self, file_path, file_name):
+        try:
+            docs=self.doc_processing.split_text(file_path, file_name)
+            self.docsearch.add_documents(docs)
+            return 'Successfully indexed the document'
+        except Exception as e:
+            print(f"Error splitting PDF text: {e}")
+
+    def _qna_helper(self, query, context):
+        res = self.openai_client.chat.completions.create(model=self.gpt_engine_name,
+                                                messages=[
+                                                        {'role': 'system','content':f'''You are a expert qna bot. Your task is to strictly answer to user query based on the given context. If answer can't be fount in the given context then reply "Sorry, this information is out of my uploaded knowledge base, Please ask queries from Uploaded Documents.".
+Only use information in context as ground truth to answer user query. Strictly answer need to be extracted and sourced from only given above context.
+
+Follow the below steps to answer the user query:
+
+Step 1: Analyse the given user query in accordance with the given below context.
+
+Step 2: Based on the above analysis, give concise and precise answer to the given user query based on the given context. Ensure that the answer must be exclusively sourced from the given context.   
+
+context: 
+{context}
+
+++++
+
+Based on the above context, answer the user query.
+'''},  
+                                                        {'role': 'user',  'content':query}]
+                                                )
+        
+        return res.choices[0].message.content
+    
+    def qna(self, query):
+        docs = self.docsearch.similarity_search(query)
+        context=''
+        for i in docs:
+            doc_name=i.metadata['filename']
+            text=i.page_content
+            context+=f'document_name: {doc_name} \n\n' + f'text: {text} \n\n'
+        answer=self._qna_helper(query, context)
+        return answer
